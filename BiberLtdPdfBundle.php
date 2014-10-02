d83d1cbeba20402f7c70115432ab8e9aae654dda
Said İmamoğlu <said.imamoglu@biberltd.com>
2014-10-02 11:22:53 +0300

    Bugfixes

--- BiberLtdPdfBundle.php
+++ BiberLtdPdfBundle.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace BiberLtd\Bundle\PdfBundle;
+
 use BiberLtd\Bundle\PdfBundle\DependencyInjection\AutoLoad;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -8,12 +9,4 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 class BiberLtdPdfBundle extends Bundle
 
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-        $container->addCompilerPass(new AutoLoad\LoadRouters());
-    }
 }
